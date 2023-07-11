fb5500cf5e471ccb4b2b36145ab98e314796c802
Meridith Lawn <meridithlawn2@gmail.com>
2023-07-10 23:44:36 -0500

    more work in useContext, projectContext.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { Switch, Route, useHistory } from "react-router-dom";
+import {useContext} from 'react'
 
+import { ProjectContext } from '../context/projectContext'
 import SignInForm from './SignInForm'
 import SignUpForm from './SignUpForm'
 import UserHome from './UserHome'
@@ -8,27 +10,20 @@ import UserHome from './UserHome'
 function App() {
 // Code goes here!
 
-const history = useHistory()
+//this holds all of the values from useContext in projectContext
+const context = useContext(ProjectContext)
+// if you don't want/need to use all of the values, you can destructure to just one ones you need in this component
+// const {projects} = useContext(ProjectContext) -> include whichever values you want to inherit in the curly braces to destructure
 
 const [showSignInForm, setShowSignInForm] = useState(false)
-const [currentUser, setCurrentUser] = useState(null)
+
 
 
 const handleToggleForm = () => {
   setShowSignInForm(currentVal => !currentVal);
 };
 
-const handleSignoutClick= () => {
-  fetch("/api/v1/signout", {method: "DELETE"})
-    .then(res => {
-      if(res.ok) {
-        setCurrentUser(null);
-        // history.push('/authentication') the ix version
-        // following line goes to authentication route, check where I want to redirect to
-        history.push('/authentication')
-      } 
-    })
-  }
+
 
 if (!currentUser) {
   return (
--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,19 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import { BrowserRouter as Router } from "react-router-dom"
-import App from './components/App'; 
+
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from 'react-router-dom'
+import App from './components/App'; 
+import { ProjectProvider } from './context/projectContext';
 // import "./index.css";
+const root = ReactDOM.createRoot(document.getElementById('root'))
 
+root.render(
+    // <UserProvider>
+        <ProjectProvider>
+            <Router>
+                <App />
+            </Router>
+        </ProjectProvider>
+    // </UserProvider>
 
-ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
-  document.getElementById('root')
 );
