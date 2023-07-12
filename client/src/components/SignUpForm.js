037459384f52ba5d6cd7931eb83adde9d454750a
Meridith Lawn <meridithlawn2@gmail.com>
2023-07-12 00:18:19 -0500

    add onBlur and placeholders"

--- client/src/components/SignUpForm.js
+++ client/src/components/SignUpForm.js
@@ -1,4 +1,5 @@
 // import React, { useEffect, useState } from "react";
+// https://formik.org/docs/tutorial to see how to implement useContext with formik
 import {useState} from "react";
 import {useFormik} from "formik";
 import * as yup from "yup";
@@ -78,102 +79,139 @@ function SignUpForm ({saveUser, handleToggleForm}){
                     id="first_name"
                     name="first_name"
                     type="text"
+                    placeholder="first name"
                     onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
                     value={formik.values.first_name}
                 />
+                {formik.errors.first_name ? <div>{formik.errors.first_name}</div> : null}
+
             <label htmlFor="last_name">last name:</label>
                 <input
                     id="last_name"
                     name="last_name"
                     type="text"
+                    placeholder="last name"
                     onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
                     value={formik.values.last_name}
                 />
+                {formik.errors.last_name ? <div>{formik.errors.last_name}</div> : null}
+
             <label htmlFor="email">email:</label>
                 <input
                     id="email"
                     name="email"
                     type="text"
+                    placeholder="email"
                     onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
                     value={formik.values.email}
                 />
+                {formik.errors.email ? <div>{formik.errors.email}</div> : null}
+
             <label htmlFor="phone">phone:</label>
                 <input
                     id="phone"
                     name="phone"
                     type="text"
+                    placeholder="phone number"
                     onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
                     value={formik.values.phone}
                 />
+                {formik.errors.phone ? <div>{formik.errors.phone}</div> : null}
+
             <label htmlFor="username">Userame:</label>
                 <input
                     id="username"
                     name="username"
                     type="text"
+                    placeholder="username"
                     onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
                     value={formik.values.username}
                 />
+                {formik.errors.username ? <div>{formik.errors.username}</div> : null}
 
             <label htmlFor="password">Password:</label>
                 <input
                     id="password"
                     name="password"
                     type="password"
+                    placeholder="password"
                     onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
                     value={formik.values.password}
                 />
+                {formik.errors.password ? <div>{formik.errors.password}</div> : null}
 
             <label htmlFor="breed">Breed:</label>
                 <input
                     id="breed"
                     name="breed"
                     type="text"
+                    placeholder="breed"
                     onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
                     value={formik.values.breed}
                 />
+                {formik.errors.breed ? <div>{formik.errors.breed}</div> : null}
 
             <label htmlFor="age">Age:</label>
                 <input
                     id="age"
                     name="age"
                     type="number"
+                    placeholder="age"
                     onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
                     value={formik.values.age}
                 />
+                {formik.errors.age ? <div>{formik.errors.age}</div> : null}
 
             <label htmlFor="weight">Weight:</label>
                 <input
                     id="weight"
                     name="weight"
                     type="number"
+                    placeholder="weight lbs"
                     onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
                     value={formik.values.weight}
                 />
+                {formik.errors.weight ? <div>{formik.errors.weight}</div> : null}
 
             <label htmlFor="fixed">Fixed:</label>
                 <input
                     id="fixed"
                     name="fixed"
                     type="text"
+                    placeholder="spay/neuter?"
                     onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
                     value={formik.values.fixed}
                 />
+                {formik.errors.fixed ? <div>{formik.errors.fixed}</div> : null}
 
             <label htmlFor="profile_pic">Profile Picture URL:</label>
                 <input
                     id="profile_pic"
                     name="profile_pic"
                     type="text"
+                    placeholder="profile picture url"
                     onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
                     value={formik.values.profile_pic}
                 />
+                {formik.errors.profile_pic ? <div>{formik.errors.profile_pic}</div> : null}
 
                 <button type="submit">Submit</button>
 
             </form>
 
             <button onClick={handleToggleForm}>
-            Already have an account? Sign in
+            Already have an account?
             </button>
         </div>
     )
