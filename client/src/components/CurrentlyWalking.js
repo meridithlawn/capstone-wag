c108f9763529a891ea5ec921d23ae36029ca26b4
Meridith Lawn <meridithlawn2@gmail.com>
2023-07-28 00:04:10 -0400

    style user cards and currently walking list

--- client/src/components/CurrentlyWalking.js
+++ client/src/components/CurrentlyWalking.js
@@ -1,16 +1,21 @@
-import React from "react"
-import WalkerCard from "./WalkerCard"
+import React from "react";
+import WalkerCard from "./WalkerCard";
+import { Divider, List } from "@mui/material";
 
-
-function CurrentlyWalking ({currentlyWalkingFriends}) {
-    
-    const mappedWalking = currentlyWalkingFriends && currentlyWalkingFriends.map(user => <WalkerCard key={user.id} {...user}/>)
-    return  (
-        <div>
-            <h1> Currently Walking</h1>
-            {mappedWalking}
-        </div>
-    )
+function CurrentlyWalking({ currentlyWalkingFriends }) {
+  const mappedWalking = currentlyWalkingFriends && currentlyWalkingFriends.map((user) => <WalkerCard key={user.id} {...user} />);
+  return (
+    <div>
+      <h1> Currently Walking</h1>
+      <List sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
+      {mappedWalking}
+        
+       
+        
+        
+      </List>
+    </div>
+  );
 }
 
-export default CurrentlyWalking
+export default CurrentlyWalking;
